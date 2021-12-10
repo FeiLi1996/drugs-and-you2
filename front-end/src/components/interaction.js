77c9952f9d98e8e7c2e38d9d1477ae08527ddd6d
FeiLi1996 <88912209+FeiLi1996@users.noreply.github.com>
2021-12-10 17:23:58 -0500

    working on interactions

--- front-end/src/components/interaction.js
+++ front-end/src/components/interaction.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState,useContext } from 'react'
+import { GlobalContext } from '../GlobalContext'
 
 
 const Interactions = () => {
-
+    const {diseaseProfile,drugProfile} = useContext(GlobalContext)
+    let [interactionsInfo,setInteractionsInfo] =useState([
+            {
+                drugName:'test',
+                interactionDescriptionAndSeverity:[
+                                    {   
+                                        diseaseName:'Asthma',
+                                        description:'blah',
+                                        severity:'blah'
+                                    },
+                                    {   
+                                        diseaseName:'Fluid Retention',
+                                        description:'blah',
+                                        severity:'blah'
+                                    },
+                ]
+            }
+        ]);
 
     return(
 
@@ -10,7 +28,24 @@ const Interactions = () => {
         <div>
 
             <h1>Interactions</h1>
-            <h6>Please Read</h6>
+            
+            <button>Check Interactions</button>
+            <button> Clear Interactions</button>
+
+
+            <ul>
+                <span>{drugProfile[0]}</span><span>{diseaseProfile[0]}</span><span>Severity</span>
+                <li>Description</li>
+            </ul>
+
+            <ul>
+                <span>Drug name</span><span>Disease Name</span><span>Severity</span>
+                <li>Description</li>
+            </ul>
+            <ul>
+                <span>{interactionsInfo[0].drugName}</span><span>{interactionsInfo[0].interactionDescriptionAndSeverity[0].diseaseName}</span><span>Severity</span>
+                <li>Description</li>
+            </ul>
         </div>
     )
 
